c2bb91973fcead63ef98e92f935cc73d947709c3
isoppp <prog@d2deck.com>
2019-03-05 00:04:17 +0900

    Added user model endpoints and set the permissions

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,23 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -8,6 +25,19 @@ class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def save(self, *args, **kwargs):
+        """User the `pygments` library to create a highlighted HTML representation of code snippet"""
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        frontmatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, frontmatter)
+        super(Snippet, self).save(*args, **kwargs)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -15,3 +45,4 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("snippets.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("snippets.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+]
