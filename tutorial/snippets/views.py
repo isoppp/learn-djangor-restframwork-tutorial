104a02ae0d80128ae1d7f9e87494895cb0d0dcbf
isoppp <prog@d2deck.com>
2019-03-05 01:04:04 +0900

    Re-factored using ViewSets

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,14 +4,10 @@ from snippets.permissions import IsOwnerOrReadOnly
 
 from django.contrib.auth.models import User
 
-from rest_framework import generics, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework import permissions, renderers, viewsets
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
 
 
 @api_view(["GET"])
@@ -24,48 +20,27 @@ def api_root(request, format=None):
     )
 
 
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """This viewset automatically provides list and detail actions"""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class SnippetList(generics.ListCreateAPIView):
-    """List all code snippets, or create a new snippet."""
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `lsit`, `create`, `retrieve`, `update` and `destroy` actions.
+    Additionally we also provide an extra `highlight` action.
+    """
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    def save(self, *args, **kwargs):
-        """User the `pygments` library to create a highlighted HTML representation of code snippet"""
-        lexer = get_lexer_by_name(self.language)
-        linenos = "table" if self.linenos else False
-        options = {"title": self.title} if self.title else {}
-        frontmatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
-        self.highlighted = highlight(self.code, lexer, frontmatter)
-        super(Snippet, self).save(*args, **kwargs)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    """Retrieve, update or delete a code snippet."""
-
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
